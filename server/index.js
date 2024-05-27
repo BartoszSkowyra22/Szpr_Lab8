7009afa21b115c8592ac8754bfbadd86d2f94981
BartoszSkowyra <skowyrab@gmail.com>
2024-05-27 23:53:51 +0200

    Changes for the ARM structure and fixing the cors error

--- server/index.js
+++ server/index.js
@@ -8,12 +8,14 @@ const tokenVerification = require('./middleware/tokenVerification')
 const connection = require('./db')
 connection()
 
+app.use(cors())
+
 //middleware
 app.get("/api/users/",tokenVerification)
 app.get("/api/users/myUser",tokenVerification)
 app.get("/api/users/delete",tokenVerification)
 app.use(express.json())
-app.use(cors())
+
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`))
 
